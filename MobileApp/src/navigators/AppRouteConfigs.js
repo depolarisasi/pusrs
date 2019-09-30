4d2785abaec97aa969e3d49daa43f62745136150
justinushermawan <justinushermawan@gmail.com>
2019-09-30 11:07:29 +0700

    Privacy terms screen

--- MobileApp/src/navigators/AppRouteConfigs.js
+++ MobileApp/src/navigators/AppRouteConfigs.js
@@ -4,12 +4,14 @@ import {
 import LoggedOut from './../screens/LoggedOut';
 import LogIn from './../screens/LogIn';
 import SignUp from './../screens/SignUp';
+import PrivacyTerms from './../screens/PrivacyTerms';
 import Home from './../screens/Home';
 
 const AppRouteConfigs = createStackNavigator({
   LoggedOut: { screen: LoggedOut },
   LogIn: { screen: LogIn },
   SignUp: { screen: SignUp },
+  PrivacyTerms: { screen: PrivacyTerms },
   Home: { screen: Home },
 });
 
