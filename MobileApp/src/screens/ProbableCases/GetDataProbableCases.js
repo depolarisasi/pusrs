f7b04bf62048b015d75d6512d8ef55a44a453a32
it.handy.caksoftware <handikadwiputradev@gmail.com>
2020-03-23 21:31:31 +0700

    fix load Maps (Mosquito Cases and probable Cases)

--- MobileApp/src/screens/ProbableCases/GetDataProbableCases.js
+++ MobileApp/src/screens/ProbableCases/GetDataProbableCases.js
@@ -4,7 +4,7 @@
  */
 
 const urlProbableCases =
-    'https://services6.arcgis.com/KKQJ8UH8yQZJCyc5/ArcGIS/rest/services/Probable_Cases/FeatureServer/0/applyEdits';
+    'https://services6.arcgis.com/KKQJ8UH8yQZJCyc5/ArcGIS/rest/services/Probable_Cases/FeatureServer/0/query?where=1=1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=json';
 
 export async function getDataProbableCases() {
     let result = await fetch(urlProbableCases).then(response =>
